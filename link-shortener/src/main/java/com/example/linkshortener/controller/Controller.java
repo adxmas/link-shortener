f8b0efa67d4887bdab7960e94b7f527e051ef52e
adxmas <47560557+adxmas@users.noreply.github.com>
2020-06-26 19:06:48 +0300

    Add files via upload
    added:
    thymeleaf
    html pages
    working encoding/decoding

--- link-shortener/src/main/java/com/example/linkshortener/controller/Controller.java
+++ link-shortener/src/main/java/com/example/linkshortener/controller/Controller.java
@@ -6,12 +6,16 @@ import com.example.linkshortener.model.Url;
 import com.example.linkshortener.service.UrlConversion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+@org.springframework.stereotype.Controller
 public class Controller {
 
     @Autowired
@@ -19,19 +23,75 @@ public class Controller {
 
     @Autowired
     UrlConversion urlConversion;
-    
-    @GetMapping("all")
+
+    // starts webpage
+    @RequestMapping(value = "/start", method = RequestMethod.GET)
+    public String start(Model model){
+        Url startUrl = urlRepositary.findById(0).orElse(null);
+        model.addAttribute("Url", startUrl);
+        return "start";
+    }
+
+    // adds url to database and encodes it
+    @RequestMapping(value = "/saveurl")
+    public String saveUrl(Url url, Model model){
+        urlRepositary.save(url);
+        //model.addAttribute("allLinks", urlRepositary.findAll());
+        //return "all-links";
+        model.addAttribute("encodedUrl", "shortcut.co/"+urlConversion.encode(url.getuId()));
+        return "encode";
+    }
+
+    // returns encoded url to its previous form
+    @RequestMapping(value = "/decode")
+    //public String decodeUrl(Url url, Model model){
+    public String decodeUrl(Url url, Model model){
+        String[] linkParts = url.getLongUrl().split("/", 2);
+        String url24 = urlRepositary.findById(urlConversion.decode(linkParts[1])).orElse(null).getLongUrl();
+        model.addAttribute("decodedUrl", url24);
+        return "decode";
+    }
+
+
+    //random operations
+    /*
+    @GetMapping("/encode/{uId}")
+    public String newLink(@PathVariable("uId") int uId, Model model) {
+        String encodedUrl = urlConversion.encode(uId);
+        model.addAttribute("encodedUrl", encodedUrl);
+        return "encode";
+    }
+
+
+    @PostMapping("/add")
+    public String postUrl(Model model){
+        model.addAttribute("allLinks", urlRepositary.findAll());
+        return "all-links";
+    }
+
+
+    @GetMapping("/all")
+    @ResponseBody
     public List<Url> getAll(){
         return urlRepositary.findAll();
     }
 
-    @PostMapping("/addurl")
+    //--
+    @GetMapping("/links")
+    public String returnAll(Model model){
+        System.out.println("returnAll worked");
+        model.addAttribute("allLinks", urlRepositary.findAll());
+        return "all-links";
+    }
+
+
+    /*@PostMapping("/addurl")
     public Url addUrl(Url url) {
         urlRepositary.save(url);
         return url;
     }
 
-    @GetMapping("/encode/{uId}")
+    /*@GetMapping("/encode/{uId}")
     public String newLink(@PathVariable("uId") int uId) {
         String encodedUrl = urlConversion.encode(uId);
         return encodedUrl;
@@ -49,6 +109,13 @@ public class Controller {
         return urlRepositary.findAll();
     }
 
+    @PutMapping("/update")
+    public Url updateUrl(@RequestBody Url url){
+        Url url2 = urlRepositary.findById(url.getuId()).orElse(null);
+        url2.setuId(url.getuId());
+        url2.setLongUrl(url.getLongUrl());
+        return urlRepositary.save(url2);
+    }*/
 
 
 }
--- link-shortener/src/main/java/com/example/linkshortener/dao/UrlRepositary.java
+++ link-shortener/src/main/java/com/example/linkshortener/dao/UrlRepositary.java
@@ -7,6 +7,6 @@ import org.springframework.data.repository.query.Param;
 
 public interface UrlRepositary extends JpaRepository<Url, Integer> {
 
-    @Query("update Url url set url.longUrl = :longUrl WHERE url.uId = :pkey")
-    void setNewLink(@Param("pkey") Long id, @Param("longUrl") String longUrl);
+
+
 }
